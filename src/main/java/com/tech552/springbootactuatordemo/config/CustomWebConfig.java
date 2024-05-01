6713e179af1fbebcb53593d0b0e3a1667e1b75e6
Erik Aschenbrenner <ashburnere@gmail.com>
2024-05-01 20:21:40 +0200

    Create a custom web security configuration to secure the  endpoints.

--- src/main/java/com/tech552/springbootactuatordemo/config/CustomWebConfig.java
+++ src/main/java/com/tech552/springbootactuatordemo/config/CustomWebConfig.java
@@ -7,14 +7,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * A custom web security configuration to secure the endpoints.
+ */
 @Configuration
 public class CustomWebConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http.authorizeRequests()
-               .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
-               .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN");
+               .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll() // all users
+               .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN"); // only users with role ADMIN
        http.csrf().and().httpBasic();
 
     }
